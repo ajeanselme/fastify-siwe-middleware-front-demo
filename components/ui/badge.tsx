cc414aca5143909ff76fc081cb4df8a528be3c8e
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2026-04-02 19:34:37 +0200

    Add POST badge variant

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -11,6 +11,7 @@ const badgeVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground [a]:hover:bg-primary/80",
         get: "bg-green-100 text-green-800 font-mono text-xs uppercase font-bold",
+        post: "bg-blue-300 text-blue-800 font-mono text-xs uppercase font-bold",
         secondary:
           "bg-secondary text-secondary-foreground [a]:hover:bg-secondary/80",
         destructive:
