df76692f3b443645388d35145f5b7b4fa5d85288
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2026-04-04 19:21:22 +0200

    Allow reseting store values

--- app/_stores/jwtStore.ts
+++ app/_stores/jwtStore.ts
@@ -1,11 +1,11 @@
 import { atom } from "nanostores";
 
 export const $jwt = atom<string | null>(null);
-export function setJwt(token: string) {
+export function setJwt(token: string | null) {
   $jwt.set(token);
 }
 
 export const $refreshToken = atom<string | null>(null);
-export function setRefreshToken(token: string) {
+export function setRefreshToken(token: string | null) {
   $refreshToken.set(token);
 }
