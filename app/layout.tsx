26003ee14ea43128898294239f2cd7bed114f53b
Antoine Jeanselme <67123340+ajeanselme@users.noreply.github.com>
2026-04-06 16:57:43 +0200

    Add links in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -41,12 +42,18 @@ export default function RootLayout({
     >
       <body>
         <div className="sm:min-h-screen flex flex-col">
-          <header className="w-full h-fit  bg-surface border-b p-4 flex items-center gap-2 font-mono text-xs">
-            <span className="text-accent font-semibold">
+          <header className="w-full h-fit  bg-surface border-b p-4 flex items-center gap-2 font-mono text-xs *:transition-colors">
+            <Link href="https://antoine.sh" className="text-muted-foreground hover:text-accent">
+              antoine.sh
+            </Link>
+            <span className="text-muted-foreground/60">/</span>
+            <Link href="https://github.com/ajeanselme/fastify-siwe-middleware" className="text-muted-foreground hover:text-accent">
               siwe-middleware
+            </Link>
+            <span className="text-muted-foreground/60">/</span>
+            <span className="text-accent font-semibold">
+              front-demo
             </span>
-            <span className="text-muted">/</span>
-            <span className="text-muted-foreground">front demo</span>
           </header>
           <div className="flex flex-col sm:flex-row flex-1">
             {children}
